53c326bea263430cc2f15a290b557508d899fb26
Eugene Kaurov <eugene.kaurov@gmail.com>
2020-09-10 14:56:48 +0200

    #2 bugfix routing

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Blog;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
